660eb84ff1af1ab1f8c80d4e7de8178432455254
Róbert Lumcer <robert.lumcer@kosickaakademia.sk>
2024-11-28 14:12:51 +0100

    course Entity

---
+++ src/main/java/sk/kasv/robert/hibernate/Entity/Course.java
@@ -0,0 +1,47 @@
+package sk.kasv.robert.hibernate.Entity;
+
+import jakarta.persistence.*;
+@Entity
+@Table(name = "course")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "title")
+    private String title;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public InstructorDetail getInstructorDetail() {
+        return instructorDetail;
+    }
+
+    public void setInstructorDetail(InstructorDetail instructorDetail) {
+        this.instructorDetail = instructorDetail;
+    }
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "instructor_id")
+    private InstructorDetail instructorDetail;
+
+    public Course(){}
+    public Course(String title){
+        this.title =title;
+    }
+}
